3d9f3aa19d8fe607741fe67e455d44e59e82ac40
River <swimmingr@gmail.com>
2022-10-07 23:33:20 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+  const callApi = async()=>{ 
+    axios.get("/api").then((res)=>{console.log(res.data.test)})
+  };
+  useEffect(()=>{
+    callApi();
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
